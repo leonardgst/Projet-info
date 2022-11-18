6012cc35d39a7296a8590c9af34dff398fee1507
ColinDespouys <103693760+ColinDespouys@users.noreply.github.com>
2022-11-18 16:07:43 +0100

    Add files via upload

--- src/AbstractRecherche/recherchetrajet.py
+++ src/AbstractRecherche/recherchetrajet.py
@@ -1,6 +1,7 @@
 from abstractrecherche import AbstractRecherche
 from DataLayer.DAO.db_connexion import DBConnexion
 from creation_table import Table_recherche
+from daohistorique import DAOHistorique
 from datetime import datetime
 import requests as rq
 
@@ -10,17 +11,23 @@ class RechercheTrajet(AbstractRecherche):
         
         """Créer une instance de RechercheTrajet"""
 
-    def recherche(self, date, origine, destination, eligible = "OUI"):
+    def recherche(self, date, origine, destination, alerter : bool = False, eligible = "OUI"):
+
+        DAOHistorique.remplir("trajet", date, origine, eligible, alerter, destination)
+        
         origine_str = str(origine).replace(" ", "+")
         origine_str = origine_str.upper()
         destination_str = str(destination).replace(" ", "+")
         destination_str = destination_str.upper()
+
         date_obj = datetime.strptime(date, '%d/%m/%Y')
         jour = str(date_obj.day)
         mois = str(date_obj.month)
         annee = str(date_obj.year)
+
         url = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&refine.origine=" + origine_str + "&refine.destination=" + destination_str + "&refine.date=" + annee + "%2F" + mois +  "%2F" + jour + "&exclude.od_happy_card=" + eligible
         Table_recherche().creation_table(url)
+        
         dict = rq.get(url)
         dict2=dict.json()
         datatest=[dict2["records"][k]for k in range(len(dict2["records"]))]
--- src/AbstractRecherche/rechercheweekend.py
+++ src/AbstractRecherche/rechercheweekend.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from abstractrecherche import AbstractRecherche
 from DataLayer.DAO.db_connexion import DBConnexion
+from daohistorique import DAOHistorique
 from creation_table import Table_recherche
 from datetime import datetime
 import requests as rq
@@ -13,22 +14,29 @@ class RechercheTrajet(AbstractRecherche):
         """
 
     def recherche(self, date, origine, destination, alerter = False, eligible = "OUI"):
+        
+        DAOHistorique.remplir("trajet", date, origine, eligible, alerter, destination)
+        
         origine_str = str(origine).replace(" ", "+")
         origine_str = origine_str.upper()
         destination_str = str(destination).replace(" ", "+")
         destination_str = destination_str.upper()
         date_obj_aller = datetime.strptime(date, '%d/%m/%Y')
         date_obj_retour = date_obj_aller + timedelta(days = 2)
+
         jour_aller = str(date_obj_aller.day)
         mois_aller = str(date_obj_aller.month)
         annee_aller = str(date_obj_aller.year)
         jour_retour = str(date_obj_retour.day)
         mois_retour = str(date_obj_retour.month)
         annee_retour = str(date_obj_retour.year)
+
         url_aller = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&refine.origine=" + origine_str + "&refine.destination=" + destination_str + "&refine.date=" + annee_aller + "%2F" + mois_aller +  "%2F" + jour_aller + "&exclude.od_happy_card=" + eligible
         url_retour = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&refine.origine=" + destination_str + "&refine.destination=" +  origine_str + "&refine.date=" + annee_retour + "%2F" + mois_retour +  "%2F" + jour_retour + "&exclude.od_happy_card=" + eligible
+        
         Table_recherche().creation_table(url_aller)
         Table_recherche().creation_table(url_retour)
+        
         dict_aller = rq.get(url_aller)
         dict_aller = dict_aller.json()
         datatest_aller=[dict_aller["records"][k]for k in range(len(dict_aller["records"]))]
